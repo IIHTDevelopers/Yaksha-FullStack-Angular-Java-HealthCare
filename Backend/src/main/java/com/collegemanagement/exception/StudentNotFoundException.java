dcee7305916497ec265aa2d2ce274a3d3c6e2c48
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-06-19 11:11:05 +0530

    Delete Backend directory

--- Backend/src/main/java/com/collegemanagement/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.collegemanagement.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-	public StudentNotFoundException(String message) {
-		super(message);
-	}
-}

06c544d65996437f19c470cde8691c412e2f37c3
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-06-19 11:11:19 +0530

    Delete Frontend directory

--- Frontend/src/app/student/models/student.ts
+++
@@ -1,7 +0,0 @@
-import { Department } from "src/app/department/models/department";
-
-export class Student {
-  id: number;
-  name: string;
-  department: Department;
-}
--- Frontend/src/app/teacher/models/teacher.ts
+++
@@ -1,7 +0,0 @@
-import { Department } from "src/app/department/models/department";
-
-export class Teacher {
-  id: number;
-  name: string;
-  department: Department;
-}

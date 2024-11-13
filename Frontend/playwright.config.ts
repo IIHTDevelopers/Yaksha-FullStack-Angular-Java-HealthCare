b28ab94016ba7231ce41e38846f9fc57023f07c2
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-11-13 17:33:21 +0530

    Update playwright.config.ts

--- Frontend/playwright.config.ts
+++ Frontend/playwright.config.ts
@@ -1,38 +1,56 @@
-import { defineConfig, devices } from "@playwright/test";
+const { defineConfig, devices } = require('@playwright/test');
 
-export default defineConfig({
-  testDir: "./src/tests",
-  retries: 1,
-  timeout: 240000,
-  expect: {
-    timeout: 100000,
-  },
-  use: {
-    baseURL: "https://healthapp.yaksha.com",
-    trace: "on",
-    headless: false,
-    screenshot: "only-on-failure",
-    video: "retry-with-video",
-  },
-
+module.exports = defineConfig({
   projects: [
     {
-      name: "chromium",
+      name: 'playwright-tests',
       use: {
-        ...devices["Desktop Chrome"],
-        viewport: { width: 1920, height: 1080 },
-        launchOptions: {
-          args: ["--disable-web-security"],
-        },
+        ...devices['Desktop Chrome'],
       },
     },
   ],
-
-  reporter: [
-    ["list"], // Default console output
-    ["./jest/my-playwright-reporter", { /* optional configuration */ }]
+  reporters: [
+    // Use the custom reporter by providing its path
+    ['./jest/custom-reporter.js'],
   ],
 });
+
+
+// import { defineConfig, devices } from "@playwright/test";
+
+// export default defineConfig({
+//   testDir: "./src/tests",
+//   retries: 1,
+//   timeout: 240000,
+//   expect: {
+//     timeout: 100000,
+//   },
+//   use: {
+//     baseURL: "https://healthapp.yaksha.com",
+//     trace: "on",
+//     headless: false,
+//     screenshot: "only-on-failure",
+//     video: "retry-with-video",
+//   },
+
+//   projects: [
+//     {
+//       name: "chromium",
+//       use: {
+//         ...devices["Desktop Chrome"],
+//         viewport: { width: 1920, height: 1080 },
+//         launchOptions: {
+//           args: ["--disable-web-security"],
+//         },
+//       },
+//     },
+//   ],
+
+//   reporter: [
+//     ["list"], // Default console output
+//     ["./jest/my-playwright-reporter", { /* optional configuration */ }]
+//   ],
+// });
 
 
 // import { defineConfig, devices } from "@playwright/test";

cdd4440d10b51669b727e43add55abb492aa8efb
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-11-21 16:54:02 +0530

    Delete Backend directory

--- Backend/src/main/java/com/healthcare/exception/DoctorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.healthcare.exception;
-
-public class DoctorNotFoundException extends RuntimeException {
-	public DoctorNotFoundException(String message) {
-		super(message);
-	}
-}
--- Backend/src/main/java/com/healthcare/exception/HospitalNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.healthcare.exception;
-
-public class HospitalNotFoundException extends RuntimeException {
-	public HospitalNotFoundException(String message) {
-		super(message);
-	}
-}
